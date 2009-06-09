7c3fabe386fd57323c5e248f78724431734e2c10
Ryuji Matsumura <ryuji@mgiken.com>
2009-06-09 23:38:34 +0900

    fixed contents of help

--- lib/peep/command/keybindings.py
+++ lib/peep/command/keybindings.py
@@ -13,48 +13,6 @@ __all__ = ['get']
 def get(mode, key):
   return KEYBINDINGS.get(mode).get(key)
 
-@bindkey(MODE.UNREAD, 'q')
-@confirm(u'Are you sure you want to quit?')
-def quit(app):
-  'exit peep'
-  sys.exit()
-
-@bindkey(MODE.HELP, 'q')
-def back_(app):
-  'back to the previous mode'
-  {MODE.UNREAD: switch_unread_mode,
-   MODE.BROWSE: switch_browse_mode}.get(app.prev_mode)(app)
-
-@bindkey(MODE.UNREAD, 'r')
-def refresh(app):
-  'refreshes the unread items'
-  app.reader.clear_cache()
-  app.ui.grid_panel.clear()
-  switch_unread_mode(app)
-
-@bindkey(MODE.BROWSE, 'u', 'q')
-@loading
-@update_status
-def switch_unread_mode(app):
-  'display the unread items'
-  app.mode = MODE.UNREAD
-  entries = app.reader.get_unread_entries()
-  if entries:
-    app.ui.grid_panel.update(entries)
-  else:
-    app.ui.grid_panel.clear()
-    raise Message('Your reading list has no unread items.')
-
-@bindkey(MODE.UNREAD, '\n')
-@loading
-@update_status
-def switch_browse_mode(app):
-  'dispay the content of the selected item'
-  app.mode = MODE.BROWSE
-  entry = get_selected_entry(app)
-  app.ui.browse_panel.update(entry)
-  app.reader.set_read(entry)
-
 @bindkey(MODE.UNREAD, 'j')
 def next(app):
   'selects the next item in the list'
@@ -67,33 +25,43 @@ def prev(app):
 
 @bindkey(MODE.BROWSE, 'j')
 def next_browse(app):
-  'display the next item'
+  'show the next item'
   if next(app): switch_browse_mode(app)
 
 @bindkey(MODE.BROWSE, 'k')
 def prev_browse(app):
-  'display the previous item'
+  'show the previous item'
   if prev(app): switch_browse_mode(app)
 
 @bindkey(MODE.BROWSE, ' ')
 def down(app):
-  'display the next page'
+  'show the next page'
   app.ui.browse_panel.down(get_selected_entry(app))
-
-@bindkey(MODE.BROWSE, 'J', '\n')
-def down1(app):
-  'go down one line'
-  app.ui.browse_panel.down(get_selected_entry(app), 1)
 
 @bindkey(MODE.BROWSE, '-')
 def up(app):
   'go back to the previous page'
   app.ui.browse_panel.up(get_selected_entry(app))
 
+@bindkey(MODE.BROWSE, 'J', '\n')
+def down1(app):
+  'go down one line'
+  app.ui.browse_panel.down(get_selected_entry(app), 1)
+
 @bindkey(MODE.BROWSE, 'K')
 def up1(app):
   'go up one line'
   app.ui.browse_panel.up(get_selected_entry(app), 1)
+
+@bindkey(MODE.UNREAD, '\n')
+@loading
+@update_status
+def switch_browse_mode(app):
+  'show the content of the selected item'
+  app.mode = MODE.BROWSE
+  entry = get_selected_entry(app)
+  app.ui.browse_panel.update(entry)
+  app.reader.set_read(entry)
 
 @bindkey(MODE.UNREAD, 'm')
 @bindkey(MODE.BROWSE, 'm')
@@ -127,6 +95,7 @@ def open(app):
 @update_status
 def open_pinned_entries(app):
   'open the pinned items by the external browser'
+  # XXX
   for i in range(int(CONF.browse.max_count)):
     if app.reader.get_pinned_count() == 0: break
     entry = app.reader.get_pinned_entries().pop(0)
@@ -137,12 +106,46 @@ def open_pinned_entries(app):
   else:
     switch_unread_mode(app)
 
+@bindkey(MODE.UNREAD, 'r')
+def reload(app):
+  'reload the list of unread items'
+  app.reader.clear_cache()
+  app.ui.grid_panel.clear()
+  switch_unread_mode(app)
+
+@bindkey(MODE.BROWSE, 'u', 'q')
+@loading
+@update_status
+def switch_unread_mode(app):
+  'show the list of unread items'
+  app.mode = MODE.UNREAD
+  entries = app.reader.get_unread_entries()
+  if entries:
+    app.ui.grid_panel.update(entries)
+  else:
+    app.ui.grid_panel.clear()
+    raise Message('Your reading list has no unread items.')
+
+@bindkey(MODE.HELP, 'q')
+def back_(app):
+  'back to the previous mode'
+  # XXX
+  {MODE.UNREAD: switch_unread_mode,
+   MODE.BROWSE: switch_browse_mode}.get(app.prev_mode)(app)
+
+@bindkey(MODE.UNREAD, 'q')
+@confirm(u'Are you sure you want to quit?')
+def quit(app):
+  'exit peep'
+  sys.exit()
+
 @bindkey(MODE.UNREAD, '?')
 @bindkey(MODE.BROWSE, '?')
-def switch_help_mode(app):
-  'show keybindings'
+def show_keybindings(app):
+  'show key bindings'
   app.ui.help_panel.update(HELPS[app.mode])
   app.mode = MODE.HELP
+  raise Message("Press 'q' to quit help.")
 
 # helper functions -----------------------------------------------------------
 
